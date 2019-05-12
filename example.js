33c2fda48613a904bf277e65813894c4e04c39f4
MatMatsu <32300160+MatMatsu@users.noreply.github.com>
2019-05-12 19:58:59 -0300

    Prueba para pull
    prueba

---
+++ example.js
@@ -0,0 +1,21 @@
+var data = {
+  "orderID": 12345,
+  "shopperName": "John Smith",
+  "shopperEmail": "johnsmith@example.com",
+  "contents": [
+    {
+      "productID": 34,
+      "productName": "SuperWidget",
+      "quantity": 1
+    },
+    {
+      "productID": 56,
+      "productName": "WonderWidget",
+      "quantity": 3
+    }
+  ],
+  "orderCompleted": true
+}
+
+/*var str = JSON.stringify(data, null, 2);
+var str2 = JSON.parse(str);*/
